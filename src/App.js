002b8ed337a3eae3a84cf7e90649fa80d8c785bb
JW Nicholson <john.nicholson812@gmail.com>
2020-03-19 16:27:34 -0400

    cleanded up code

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import { Route } from 'react-router-dom';
 import data from './data';
 import { ProductContext } from './contexts/ProductContext';
 import { CartContext } from './contexts/CartContext';
-
-
-
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -14,31 +11,28 @@ import ShoppingCart from './components/ShoppingCart';
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
-
+  
 	const addItem = item => {
 		// add the given item to the cart
-		//spread to retain current data
-		setCart([...cart, item]);
+	  setCart([...cart, item]);
 	};
-
+  
 	return (
-		<div className="App">
-			<ProductContext.Provider value={{products, addItem}} >
-			<CartContext.Provider value={{cart}} >
+	  <div className="App">
+		<ProductContext.Provider value={{ products, addItem }}>
+		  <CartContext.Provider value={cart}>
 			<Navigation cart={cart} />
-
+  
 			{/* Routes */}
-			<Route exact path="/">
-				<Products />
-			</Route>
+			<Route exact path="/" component={Products} />
+  
+			<Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
-			</CartContext.Provider>
-			</ProductContext.Provider>
-		</div>
+		  </CartContext.Provider>
+		</ProductContext.Provider>
+	  </div>
 	);
-}
-
-export default App;
+  }
+  
+  export default App;
+  
